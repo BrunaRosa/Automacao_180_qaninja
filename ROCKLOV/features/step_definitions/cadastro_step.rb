e38afad5fbe436c53e90a6cba8f3f9568c5339ff
BrunaRosa <brosa@bionexo.com>
2021-01-29 00:00:47 -0300

    Create features file

---
+++ ROCKLOV/features/step_definitions/cadastro_step.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+Dado('que acesso a página de cadastro') do
+  visit "http://localhost:3000/signup"
+end
+
+Quando('submeto o meu cadastro completo') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então('sou redirecionado para o Dashboard') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
