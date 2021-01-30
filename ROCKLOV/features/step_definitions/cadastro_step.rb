8a5c8bdf766f3d898dd60cd89fb1a09ccc3155d5
BrunaRosa <brosa@bionexo.com>
2021-01-29 23:29:40 -0300

    Add code to steps of the feature

--- ROCKLOV/features/step_definitions/cadastro_step.rb
+++ ROCKLOV/features/step_definitions/cadastro_step.rb
@@ -1,13 +1,29 @@
 # frozen_string_literal: true
 
 Dado('que acesso a página de cadastro') do
-  visit "http://localhost:3000/signup"
+  visit 'http://localhost:3000/signup'
 end
 
 Quando('submeto o meu cadastro completo') do
-  pending # Write code here that turns the phrase above into concrete actions
+  find('#fullName').set Faker::Internet.username
+  find('#email').set Faker::Internet.free_email
+  find('#password').set '1234'
+
+  click_button 'Cadastrar'
 end
 
 Então('sou redirecionado para o Dashboard') do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(page).to have_css('.dashboard')
+end
+
+Quando('submeto o meu cadastro sem o nome') do
+  find('#email').set Faker::Internet.free_email
+  find('#password').set '1234'
+
+  click_button 'Cadastrar'
+end
+
+Então('vejo a mensagem de alerta: Oops.Informe seu nome completo!') do
+  alert = find(".alert-dark")
+  expect(alert.text).to eql 'Oops. Informe seu nome completo!'
 end
