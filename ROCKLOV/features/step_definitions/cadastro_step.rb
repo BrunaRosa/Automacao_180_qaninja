65e9491a270cc7213d951766faccc1a27876c44a
BrunaRosa <brosa@bionexo.com>
2021-02-01 23:59:22 -0300

    Remove steps equals

--- ROCKLOV/features/step_definitions/cadastro_step.rb
+++ ROCKLOV/features/step_definitions/cadastro_step.rb
@@ -4,10 +4,14 @@ Dado('que acesso a página de cadastro') do
   visit 'http://localhost:3000/signup'
 end
 
-Quando('submeto o meu cadastro completo') do
-  find('#fullName').set Faker::Internet.username
-  find('#email').set Faker::Internet.free_email
-  find('#password').set '1234'
+Quando('submeto o seguinte formulário do cadastro') do |table|
+  user = table.hashes.first
+
+  MongoDB.new.remove_user(user[:email])
+
+  find('#fullName').set user[:nome]
+  find('#email').set user[:email]
+  find('#password').set user[:senha]
 
   click_button 'Cadastrar'
 end
@@ -23,11 +27,6 @@ Quando('submeto o meu cadastro sem o nome') do
   click_button 'Cadastrar'
 end
 
-Então('vejo a mensagem de alerta: Oops.Informe seu nome completo!') do
-  alert = find('.alert-dark')
-  expect(alert.text).to eql 'Oops. Informe seu nome completo!'
-end
-
 Quando('submeto o meu cadastro sem o email') do
   find('#fullName').set Faker::Internet.username
   find('#password').set '1234'
@@ -37,15 +36,10 @@ end
 
 Quando('submeto o meu cadastro com email incorreto') do
   find('#fullName').set Faker::Internet.username
-  find('#email').set "bruna@gmail"
+  find('#email').set 'bruna@gmail'
   find('#password').set '1234'
 
   click_button 'Cadastrar'
-end
-
-Então('vejo a mensagem de alerta: Oops.Informe um email válido!') do
-  alert = find('.alert-dark')
-  expect(alert.text).to eql 'Oops. Informe um email válido!'
 end
 
 Quando('submeto o meu cadastro sem a senha') do
@@ -55,7 +49,7 @@ Quando('submeto o meu cadastro sem a senha') do
   click_button 'Cadastrar'
 end
 
-Então('vejo a mensagem de alerta: Oops.Informe sua senha secreta!') do
+Então('vejo a mensagem de alerta: {string}') do |expect_alert|
   alert = find('.alert-dark')
-  expect(alert.text).to eql 'Oops. Informe sua senha secreta!'
+  expect(alert.text).to eql expect_alert
 end
---
+++ ROCKLOV/features/support/libs/mongo.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'mongo'
+
+Mongo::Logger.logger = Logger.new('./logs/mongo.log')
+
+class MongoDB
+  def remove_user(_email)
+    client = Mongo::Client.new('mongodb://rocklov-db:27017/rocklov')
+    users = client[:users]
+    users.delete_many(email: 'bruninha20_468@hotmail.com')
+  end
+end
