a84b1a955363fdab74a2fc064cef998db3836f26
BrunaRosa <brosa@bionexo.com>
2021-02-01 22:39:06 -0300

    Add validates to steps

--- ROCKLOV/features/step_definitions/cadastro_step.rb
+++ ROCKLOV/features/step_definitions/cadastro_step.rb
@@ -35,7 +35,27 @@ Quando('submeto o meu cadastro sem o email') do
   click_button 'Cadastrar'
 end
 
+Quando('submeto o meu cadastro com email incorreto') do
+  find('#fullName').set Faker::Internet.username
+  find('#email').set "bruna@gmail"
+  find('#password').set '1234'
+
+  click_button 'Cadastrar'
+end
+
 Então('vejo a mensagem de alerta: Oops.Informe um email válido!') do
   alert = find('.alert-dark')
   expect(alert.text).to eql 'Oops. Informe um email válido!'
+end
+
+Quando('submeto o meu cadastro sem a senha') do
+  find('#fullName').set Faker::Internet.username
+  find('#email').set Faker::Internet.free_email
+
+  click_button 'Cadastrar'
+end
+
+Então('vejo a mensagem de alerta: Oops.Informe sua senha secreta!') do
+  alert = find('.alert-dark')
+  expect(alert.text).to eql 'Oops. Informe sua senha secreta!'
 end
